03c8732e2111d4465e1e005b22dfc00bef0adb58
BORHAN <borhanuddin6888@gmail.com>
2022-07-02 11:22:02 +0600

    commiit

--- src/components/Header/Nav.tsx
+++ src/components/Header/Nav.tsx
@@ -1,7 +1,31 @@
-import React from 'react';
+import Link from 'next/link';
 
 const Nav = () => {
-  return <div>Nav</div>;
+  return (
+    <nav>
+      <Link color="text.primary" href="#">
+        shwapno Card
+      </Link>
+      <Link color="text.primary" href="#">
+        Bata Gift Card
+      </Link>
+      <Link color="text.primary" href="#">
+        Eid Gift Card
+      </Link>
+      <Link color="text.primary" href="#">
+        Qurbani e Haat
+      </Link>
+      <Link color="text.primary" href="#">
+        Eid Fashon Lifestyle
+      </Link>
+      <Link color="text.primary" href="#">
+        Live Hour Deal
+      </Link>
+      <Link color="text.primary" href="#">
+        শিক্ষাঙ্গন.কম
+      </Link>
+    </nav>
+  );
 };
 
 export default Nav;
